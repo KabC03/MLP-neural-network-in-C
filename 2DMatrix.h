a71bf6faca5a2fc285926fd651a0c84c41165528
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-23 09:04:19 +1000

    Update and rename Matrix.h to 2DMatrix.h

---
+++ 2DMatrix.h
@@ -0,0 +1,30 @@
+// 2D matricies in C
+
+#ifndef MATRIX2D_H
+#define MATRIX2D_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+
+
+
+typedef struct Matrix {
+
+    size_t rows;
+    size_t cols;
+
+    uint8_t *data;
+
+} Matrix;
+
+
+
+
+
+
+#endif // define
+
+
