d11afd402378f6d8a613f22ed85209f735ad6f63
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-23 11:54:10 +1000

    Update MLP.h

--- libraries/MLP.h
+++ libraries/MLP.h
@@ -1,4 +1,4 @@
-// MLP library
+// MLP library - NOTE assumes floats
 
 #ifndef MLP_H
 #define MLP_H
@@ -8,10 +8,12 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdint.h>
+#include <time.h>
+#include <limits.h>
 #include "2Dmatrix.h"
 
-
-
+bool MLP_randomise(Matrix *const matrix, float range, float min);
+bool MLP_mean_square_error(Matrix *const matrix, float expectedValue ,float *output);
 
 
 
