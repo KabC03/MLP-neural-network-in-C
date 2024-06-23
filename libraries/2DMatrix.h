6ce009ee3f50ba8abe16d3d2e4541073cd791ed4
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-23 15:52:20 +1000

    Update 2DMatrix.h

--- libraries/2DMatrix.h
+++ libraries/2DMatrix.h
@@ -16,10 +16,9 @@ bool MLP_randomise(Matrix *const matrix, float range, float min);
 bool MLP_mean_square_error(Matrix *const matrix, float expectedValue ,float *output);
 
 bool MLP_ReLu(Matrix *const arg1, Matrix *const result);
-
+bool MLP_ReLu_gradient(Matrix *const arg1, Matrix *const result);
 
 #endif // MLP_H
 
 
 
- 
