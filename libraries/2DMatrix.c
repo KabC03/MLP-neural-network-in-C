97422b2e56fbbe52715d957ea25a21b4173d7165
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-23 10:32:00 +1000

    Update 2DMatrix.c

--- libraries/2DMatrix.c
+++ libraries/2DMatrix.c
@@ -18,7 +18,7 @@ bool matrix_2D_print(Matrix *const matrix) {
         return false;
     } else {
 
-        for(size_t i = 0; i < matrix->cols; i++) {
+        for(size_t i = 0; i < matrix->rows; i++) {
             for(size_t j = 0; j < matrix->cols; j++) {
 
                 printf("%d ", (matrix->data)[(matrix->dataSize) * ((i * matrix->cols) + j)]);
@@ -84,7 +84,10 @@ bool matrix_2D_add(Matrix *const result, Matrix *const arg1, Matrix *const arg2)
     } else if(arg1->cols != arg2->cols 
     || arg1->rows != arg2->rows 
     || result->cols != arg1->cols 
-    || result->rows != arg1->rows) { //Incompatable dimensions
+    || result->rows != arg1->rows
+    || result->dataSize != arg1->dataSize
+    || arg1->dataSize != arg2->dataSize
+    ) { //Incompatable dimensions
         return false;
     } else {
 
@@ -120,7 +123,10 @@ bool matrix_2D_subtract(Matrix *const result, Matrix *const arg1, Matrix *const 
     } else if(arg1->cols != arg2->cols 
     || arg1->rows != arg2->rows 
     || result->cols != arg1->cols 
-    || result->rows != arg1->rows) { //Incompatable dimensions
+    || result->rows != arg1->rows
+    || result->dataSize != arg1->dataSize
+    || arg1->dataSize != arg2->dataSize
+    ) { //Incompatable dimensions
         return false;
     } else {
 
@@ -138,6 +144,54 @@ bool matrix_2D_subtract(Matrix *const result, Matrix *const arg1, Matrix *const 
 
 
 
+
+/**
+ * matrix_2D_multiply
+ * ===============================================
+ * Brief: Multiply two matricies
+ * 
+ * Param: *result - Result matrix
+ *        *arg1 - arg1 matrix
+ *        *arg2 - arg2 matrix
+ * Return: bool - T/F depending on if initialisation was successful
+ * 
+ */
+bool matrix_2D_multiply(Matrix *const result, Matrix *const arg1, Matrix *const arg2) {
+
+    if(result == NULL || arg1 == NULL || arg2 == NULL) {
+        return false;
+
+
+    } else if(arg1->cols != arg2->rows 
+    || result->rows != arg1->rows 
+    || result->cols != arg2->cols
+    || arg1->dataSize != arg2->dataSize
+    || arg1->dataSize != result->dataSize) { //Incompatable dimensions
+        return false;
+    } else {
+
+
+        
+        for(size_t i = 0; i < arg1->rows; i++) {
+            for(size_t j = 0; j < arg2->cols; j++) {
+
+                (result->data)[(result->dataSize) * (i * result->cols + j)] = 0; 
+                //Zero before doing the sum - prevents needed additional sum temp variable
+                for(size_t k = 0; k < arg1->cols; k++) {
+
+                    (result->data)[(result->dataSize) * ((i * result->cols + j))] += (arg1->data)[(arg1->dataSize) * (k + (arg1->cols * i))] + (arg2->data)[(arg2->dataSize) * (j + (arg2->cols * k))];
+
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+
+
+//Transpose, Dot product, ReLU + Derivative, Sigmoid + Derivative
 
 
 
