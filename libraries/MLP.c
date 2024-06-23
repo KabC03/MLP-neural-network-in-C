34c71c870de76cb5d48ef8162d92b70958883f91
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-23 12:22:06 +1000

    Update MLP.c

--- libraries/MLP.c
+++ libraries/MLP.c
@@ -64,35 +64,6 @@ bool MLP_mean_square_error(Matrix *const matrix, float expectedValue ,float *out
 
 
 
-/**
- * MLP_mean_square_error
- * ===============================================
- * Brief: Calculate the MSE of a matrix
- * 
- * Param: *matrix - matrix of interest
- *        expectedValue - expected value
- *        *output - output data pointer
- * Return: bool - T/F depending on if initialisation was successful
- * 
- */
-bool MLP_mean_square_error(Matrix *const matrix, float expectedValue ,float *output) {
-
-    if(matrix == NULL || output == NULL) {
-        return false;
-    } else if(matrix->data == NULL) {
-        return false;
-    } else {
-
-        (*output) = 0;
-        for(size_t i = 0; i < (matrix->rows * matrix->cols); i++) {
-
-            (*output) += square(((matrix->data)[i * matrix->dataSize]) - expectedValue);
-        }
-        (*output) /= (matrix->rows * matrix->cols);
-
-    }
-    return true;
-}
 
 
 
