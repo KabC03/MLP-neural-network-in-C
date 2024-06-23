effba18c9aa2722e69f20d1a5b1d4d2e6a7e6b9c
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-23 11:29:01 +1000

    Update MLP.h

--- libraries/MLP.h
+++ libraries/MLP.h
@@ -1,1 +1,21 @@
+// MLP library
+
+#ifndef MLP_H
+#define MLP_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+#include "2Dmatrix.h"
+
+
+
+
+
+
+
+#endif // MLP_H
+
 
