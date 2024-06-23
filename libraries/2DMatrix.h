745de98ec51f42ae79a009f084947f3df5ec8da8
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-23 10:42:29 +1000

    Update 2DMatrix.h

--- libraries/2DMatrix.h
+++ libraries/2DMatrix.h
@@ -25,7 +25,7 @@ bool matrix_2D_print(Matrix *const matrix);
 bool matrix_2D_initialise(Matrix *const matrix, size_t rows, size_t cols, void *data, size_t dataSize);
 bool matrix_2D_add(Matrix *const result, Matrix *const arg1, Matrix *const arg2);
 bool matrix_2D_subtract(Matrix *const result, Matrix *const arg1, Matrix *const arg2);
-
+bool matrix_2D_multiply(Matrix *const result, Matrix *const arg1, Matrix *const arg2);
 
 
 #endif // MATRIX2D_H
