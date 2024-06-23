6698005eacd7347f3cb9d2717194bbf1b531e034
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-23 15:48:32 +1000

    Update 2DMatrix.c

--- libraries/2DMatrix.c
+++ libraries/2DMatrix.c
@@ -1,269 +1,103 @@
-#include "2Dmatrix.h"
-
-
-
-
+#include "MLP.h"
+#define square(value) (value * value)
 
 /**
- * matrix_2D_print
+ * MLP_randomise
  * ===============================================
- * Brief: Print a 2D matrix of integers (used for debugging)
+ * Brief: Generates a random matrix
  * 
  * Param: *matrix - matrix of interest
+ *        range - range from minimum
+ *        min - min value in matrix
  * Return: bool - T/F depending on if initialisation was successful
  * 
  */
-bool matrix_2D_print(Matrix *const matrix) {
+bool MLP_randomise(Matrix *const matrix, float range, float min) {
 
     if(matrix == NULL) {
         return false;
+    } else if(matrix->data == NULL) {
+        return false;
     } else {
 
-        for(size_t i = 0; i < matrix->rows; i++) {
-            for(size_t j = 0; j < matrix->cols; j++) {
-
-                printf("%d ", (matrix->data)[(matrix->dataSize) * ((i * matrix->cols) + j)]);
-            }
-            printf("\n");
+        srand(time(NULL));
+        for(size_t i = 0; i < (matrix->cols * matrix->rows); i++) {
+            matrix->data[(matrix->dataSize) * i] = (min + (range * (float)rand())/RAND_MAX);
         }
-    }
 
+    }
     return true;
 }
 
 
 
+
 /**
- * matrix_2D_initialise
+ * MLP_mean_square_error
  * ===============================================
- * Brief: Initialise a 2D matrix
+ * Brief: Calculate the MSE of a matrix
  * 
  * Param: *matrix - matrix of interest
- *        rows - rows of the matrix
- *        cols - columns of the matrix
- *        dataSize - sizeof(element) in bytes
+ *        expectedValue - expected value
+ *        *output - output data pointer
  * Return: bool - T/F depending on if initialisation was successful
  * 
  */
-bool matrix_2D_initialise(Matrix *const matrix, size_t rows, size_t cols, size_t dataSize) {
+bool MLP_mean_square_error(Matrix *const matrix, float expectedValue ,float *output) {
 
-    if(matrix == NULL || rows == 0 || cols == 0 || dataSize == 0) {
+    if(matrix == NULL || output == NULL) {
+        return false;
+    } else if(matrix->data == NULL) {
         return false;
     } else {
 
-        matrix->cols = cols;
-        matrix->rows = rows;
-        matrix->dataSize = dataSize;
+        (*output) = 0;
+        for(size_t i = 0; i < (matrix->rows * matrix->cols); i++) {
 
-        matrix->data = malloc(dataSize * rows * cols);
-        if(matrix->data == NULL) {
-            return false;
+            (*output) += square(((matrix->data)[i * matrix->dataSize]) - expectedValue);
         }
-    }
+        (*output) /= (matrix->rows * matrix->cols);
 
+    }
     return true;
 }
 
 
 
 
-/**
- * matrix_2D_set
- * ===============================================
- * Brief: Set or resize a 2D matrix
- * 
- * Param: *matrix - matrix of interest
- *        rows - rows of the matrix
- *        cols - columns of the matrix
- *        *data - data to put into the matrix
- *        dataSize - sizeof(element) in bytes
- * Return: bool - T/F depending on if initialisation was successful
- * 
- */
-bool matrix_2D_set(Matrix *const matrix, size_t rows, size_t cols, void *data, size_t dataSize) {
-
-    if(matrix == NULL || rows == 0 || cols == 0 || data == NULL || dataSize == 0) {
-        return false;
-    } else {
-
-        matrix->cols = cols;
-        matrix->rows = rows;
-        matrix->dataSize = dataSize;
-
-
-        matrix->data = realloc(matrix->data, dataSize * rows * cols);
-        if(matrix->data == NULL) {
-            return false;
-        }
-        memcpy(matrix->data, data, dataSize * rows * cols);
-    }
-
-    return true;
-}
-
-
-
-
-
-
+//Activation functions
 
 
 
 /**
- * matrix_2D_add
+ * MLP_ReLu
  * ===============================================
- * Brief: Add two matricies
+ * Brief: Calculates ReLu on a matrix
  * 
- * Param: *result - Result matrix
- *        *arg1 - arg1 matrix
- *        *arg2 - arg2 matrix
+ * Param: *result - Output matrix with ReLu values
+ *        *arg1 - matrix to calculate ReLu on
  * Return: bool - T/F depending on if initialisation was successful
  * 
  */
-bool matrix_2D_add(Matrix *const result, Matrix *const arg1, Matrix *const arg2) {
-
-    if(result == NULL || arg1 == NULL || arg2 == NULL) {
-        return false;
-
-
-    } else if(arg1->cols != arg2->cols 
-    || arg1->rows != arg2->rows 
-    || result->cols != arg1->cols 
-    || result->rows != arg1->rows
-    || result->dataSize != arg1->dataSize
-    || arg1->dataSize != arg2->dataSize
-    ) { //Incompatable dimensions
-        return false;
-    } else {
-
-        for(size_t i = 0; i < (result->rows) * (result->cols); i++) {
-
-            (result->data)[i * result->dataSize] = (arg1->data)[i * arg1->dataSize] + (arg2->data)[i * arg2->dataSize];
-        }
-
-    }
-
-    return true;
-}
-
-
-
-/**
- * matrix_2D_subtract
- * ===============================================
- * Brief: Subtract two matricies
- * 
- * Param: *result - Result matrix
- *        *arg1 - arg1 matrix
- *        *arg2 - arg2 matrix
- * Return: bool - T/F depending on if initialisation was successful
- * 
- */
-bool matrix_2D_subtract(Matrix *const result, Matrix *const arg1, Matrix *const arg2) {
-
-    if(result == NULL || arg1 == NULL || arg2 == NULL) {
-        return false;
-
-
-    } else if(arg1->cols != arg2->cols 
-    || arg1->rows != arg2->rows 
-    || result->cols != arg1->cols 
-    || result->rows != arg1->rows
-    || result->dataSize != arg1->dataSize
-    || arg1->dataSize != arg2->dataSize
-    ) { //Incompatable dimensions
-        return false;
-    } else {
-
-        for(size_t i = 0; i < (result->rows) * (result->cols); i++) {
-
-            (result->data)[i * result->dataSize] = (arg1->data)[i * arg1->dataSize] - (arg2->data)[i * arg2->dataSize];
-        }
-
-    }
-
-    return true;
-}
-
-
-
-
-
-
-/**
- * matrix_2D_multiply
- * ===============================================
- * Brief: Multiply two matricies
- * 
- * Param: *result - Result matrix
- *        *arg1 - arg1 matrix
- *        *arg2 - arg2 matrix
- * Return: bool - T/F depending on if initialisation was successful
- * 
- */
-bool matrix_2D_multiply(Matrix *const result, Matrix *const arg1, Matrix *const arg2) {
-
-    if(result == NULL || arg1 == NULL || arg2 == NULL) {
-        return false;
-
-
-    } else if(arg1->cols != arg2->rows 
-    || result->rows != arg1->rows 
-    || result->cols != arg2->cols
-    || arg1->dataSize != arg2->dataSize
-    || arg1->dataSize != result->dataSize) { //Incompatable dimensions
-        return false;
-    } else {
-
-
-        
-        for(size_t i = 0; i < arg1->rows; i++) {
-            for(size_t j = 0; j < arg2->cols; j++) {
-
-                (result->data)[(result->dataSize) * (i * result->cols + j)] = 0; 
-                //Zero before doing the sum - prevents needed additional sum temp variable
-                for(size_t k = 0; k < arg1->cols; k++) {
-
-                    (result->data)[(result->dataSize) * ((i * result->cols + j))] += 
-                    (arg1->data)[(arg1->dataSize) * (k + (arg1->cols * i))] * 
-                    (arg2->data)[(arg2->dataSize) * (j + (arg2->cols * k))];
-
-                }
-            }
-        }
-    }
-
-    return true;
-}
-
-
-
-
-/**
- * matrix_2D_transpose
- * ===============================================
- * Brief: Transpose a matrix
- * 
- * Param: *result - Result matrix
- *        *arg1 - arg1 matrix
- * Return: bool - T/F depending on if initialisation was successful
- * 
- */
-bool matrix_2D_transpose(Matrix *const result, Matrix *const arg1) {
+bool MLP_ReLu(Matrix *const arg1, Matrix *const result) {
 
     if(result == NULL || arg1 == NULL) {
         return false;
-    } else if(result->cols != arg1->rows || result->rows != arg1->cols) {
+    } else if(result->data == NULL || arg1->data == NULL || arg1->cols != result->cols || arg1->rows != result->rows) {
         return false;
     } else {
 
-        for(size_t i = 0; i < arg1->rows; i++) {
-            for(size_t j = 0; j < arg1->cols; j++) {
+        for(size_t i = 0; i < (arg1->cols * arg1->rows); i++) {
 
-                (result->data)[(result->dataSize) * ((result->cols * j) + i)] = 
-                (arg1->data)[(arg1->dataSize) * ((arg1->cols * i) + j)];
+            if(*(float*)(&((arg1->data)[(arg1->dataSize) * i])) < 0) {
+                *(float*)(&((arg1->data)[(arg1->dataSize) * i])) = 0;
+
+
+            } else {
+                continue;
             }
         }
+
     }
     return true;
 }
@@ -273,7 +107,16 @@ bool matrix_2D_transpose(Matrix *const result, Matrix *const arg1) {
 
 
 
-//Transpose, Dot product, ReLU + Derivative, Sigmoid + Derivative
+
+
+
+
+
+
+
+
+
+
 
 
 
