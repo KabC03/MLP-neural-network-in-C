88d134a18b6e936de544f27d9c040b7427ae0ffa
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-23 11:12:38 +1000

    Update 2DMatrix.h

--- libraries/2DMatrix.h
+++ libraries/2DMatrix.h
@@ -23,6 +23,7 @@ typedef struct Matrix {
 
 bool matrix_2D_print(Matrix *const matrix);
 bool matrix_2D_initialise(Matrix *const matrix, size_t rows, size_t cols, void *data, size_t dataSize);
+bool matrix_2D_resize(Matrix *const matrix, size_t rows, size_t cols, void *data, size_t dataSize);
 bool matrix_2D_add(Matrix *const result, Matrix *const arg1, Matrix *const arg2);
 bool matrix_2D_subtract(Matrix *const result, Matrix *const arg1, Matrix *const arg2);
 bool matrix_2D_multiply(Matrix *const result, Matrix *const arg1, Matrix *const arg2);
