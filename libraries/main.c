95ef4e322d1fed71e01d1d15551ddbefb543e2ef
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-24 18:07:01 +1000

    Update main.c

--- libraries/main.c
+++ libraries/main.c
@@ -3,8 +3,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
-
-
+#include <math.h>
+#define FILE_BUFFER_SIZE 100
 
 
 
@@ -27,7 +27,7 @@ typedef struct BMPInfo {
     int32_t width;
     int32_t height;
     uint16_t planes;
-    uint16_t bits;
+    uint16_t bitsPerPixel;
     uint32_t compression;
     uint32_t imagesize;
     int32_t xresolution;
@@ -40,52 +40,125 @@ typedef struct BMPInfo {
 
 
 
-//Read into a column matrix
+
+
+
+
+
+//Read into a column matrix (Greyscale and load)
 bool read_image_into_matrix(Matrix *const matrix, BMPHeader *const BMPheader, BMPInfo *const BMPinfo) {
 
-    matrix_2D_initialise(matrix, BMPinfo->width * BMPinfo->height, 1, sizeof(int));
+    
 
 
+    return true;
+}
+
+
+
+bool train_MLP(char *dirName) {
+
+
+
+    //Image training
+    BMPHeader BMPheader;
+    BMPInfo BMPinfo;
+    FILE *image = NULL;
+    Matrix inputLayer = {0,0,0,0};
+    char fileNameBuffer[FILE_BUFFER_SIZE];
+
+    while(1) { //For file in directory (Do in C++)
+
+
+
+        //Assign file name here
+
+
+
+        image = fopen(fileNameBuffer, "rb");
+        if(image == NULL) {
+            printf("Failed to open image '%s'\n",fileNameBuffer);
+            return false;
+        } 
+        fread(&BMPheader, sizeof(BMPHeader), 1, image);
+        fread(&BMPinfo, sizeof(BMPinfo), 1, image);
+        if(BMPheader.type != 0x4D42) {
+            printf("Folder should only contain BMP files - '%s'\n",fileNameBuffer);
+            fclose(image);
+            return false;
+        }
+
+
+        if(BMPinfo.height * inputLayer.cols != inputLayer.rows || BMPinfo.width != 1) { 
+            //Either first time load or incorrect image size
+
+            if(inputLayer.rows == 0 && inputLayer.cols == 0) { //First load therefore initialise matrix
+                if(matrix_2D_initialise(&inputLayer, abs(BMPinfo.height * BMPinfo.width), 1, sizeof(float)) == false) {
+                    printf("Failed to initialise input layer for '%s'\n",fileNameBuffer);
+                    return false;
+                }
+
+            } else {
+                printf("Image '%s' of incorrect dimensions\n",fileNameBuffer);
+                return false;
+            }
+        }
+    }
+
+    fclose(image);
+    return true;
 
 }
 
 
 
 
-int main(void) {
+//TODO - Filesystem libray in C++ -> call in this C file to get all files in dir
+//Read image into input layer of neurons
 
-    
-    BMPHeader BMPheader;
-    BMPInfo BMPinfo;
+int main(int argc, char *argv[]) {
 
-    FILE *image = fopen("./logo.bmp", "rb");
-    if(image == NULL) {
-        printf("Failed to open image\n");
-        return 1;
-    } 
+    //Command line
+    /*
+    Format: ./main.c ./Directory -flags 
 
-    fread(&BMPheader, sizeof(BMPHeader), 1, image);
-    fread(&BMPinfo, sizeof(BMPinfo), 1, image);
+        ./Directory - directory of bmp
 
-    if(BMPheader.type != 0x4D42) {
-        printf("Folder should only contain BMP files\n");
-        fclose(image);
+        Flags (max one):
+        -t : train Train on the dataset
+        -r : run  Run on the dataset
+    */
+
+
+    if(argc != 3) {
+        printf("Format: ./main Path -flags\n\n");
+        
+        printf("        Path - directory of bmp images\n\n");
+        printf("        Flags (max one):\n");
+        printf("            -t : train Train on the dataset\n");
+        printf("            -r : run  Run on the dataset\n");
         return 1;
     }
 
 
-    Matrix inputLayer;
-    printf("Width: %d\n", BMPinfo.width);
-    printf("Height: %d\n", BMPinfo.height);
-    printf("Bit Depth: %d\n", BMPinfo.bits);
+
+
+
+    //Prediction
 
 
 
 
-    fclose(image);
+
+
+
+
+
+
+
+
     return 0;
 }
-
 
 
 
